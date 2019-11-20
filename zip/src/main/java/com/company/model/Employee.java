55f730164402f3373e9a2d43e27518619949b37a
enes <enes@enes>
2019-11-20 11:55:49 +0300

    comment

--- zip/src/main/java/com/company/model/Employee.java
+++ zip/src/main/java/com/company/model/Employee.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity								//entity oluşturduk
 public class Employee {
 
     @Id
